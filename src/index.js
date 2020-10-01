753ec9afca39a481deb465694c5cc176cfdf1062
AliaksandrFiliou <Aliaksandr.Filiou2019@Gmail.com>
2020-10-01 21:56:24 +0300

    feat: add min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+    result = 0;
+    if (array !== undefined && array.length > 0) {
+        for (i = 0; i < array.length; i++) {
+            if (array[i] < result) {
+            result = array[i];
+            }
+        }
+    return result;
+    } else {
+        return 0;
+    } 
 }
 
 exports.max = function max (array) {
-  return 0;
+    result = 0;
+    if (array !== undefined && array.length > 0) {
+        for (i = 0; i < array.length; i++) {
+            if (array[i] > result) {
+            result = array[i];
+        }
+    }
+    return result;
+    } else {
+        return 0;
+    } 
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    result = 0;
+    if (array !== undefined && array.length > 0) {
+        for (i = 0; i < array.length; i++) {
+        result += array[i];
+        }
+    result = result / array.length;
+    return result;
+    } else {
+        return 0;
+    }    
 }
